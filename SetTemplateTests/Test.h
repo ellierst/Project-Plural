d06d4490fead5b06edaf0ad7b02dd3bab776980f
Sumbrsh <165149947+Sumbrsh@users.noreply.github.com>
2024-04-02 20:07:17 +0300

    Add files via upload

---
+++ SetTemplateTests/Test.h
@@ -0,0 +1,98 @@
+#pragma once
+#include "gtest/gtest.h"
+#include "Template.h"
+
+TEST(SetTest, AddTest) {
+    Set<int> set;
+    set.add(5);
+    EXPECT_EQ(set.getSize(), 1);
+    EXPECT_TRUE(set.contains(5));
+}
+
+TEST(SetTest, DefaultConstructorTest) {
+    Set<int> set;
+    EXPECT_EQ(set.getSize(), 0);
+}
+
+TEST(SetTest, CopyConstructorTest) {
+    Set<int> set;
+    set.add(1);
+    set.add(2);
+    Set<int> copiedSet(set);
+    EXPECT_EQ(copiedSet.getSize(), set.getSize());
+    EXPECT_TRUE(copiedSet.contains(1));
+    EXPECT_TRUE(copiedSet.contains(2));
+}
+
+TEST(SetTest, AssignmentOperatorTest) {
+    Set<int> set1;
+    set1.add(1);
+    set1.add(2);
+    Set<int> set2;
+    set2 = set1;
+    EXPECT_EQ(set2.getSize(), set1.getSize());
+    EXPECT_TRUE(set2.contains(1));
+    EXPECT_TRUE(set2.contains(2));
+}
+
+TEST(SetTest, RemoveTest) {
+    Set<int> set;
+    set.add(5);
+    set.remove(5);
+    EXPECT_EQ(set.getSize(), 0);
+    EXPECT_FALSE(set.contains(5));
+}
+
+TEST(SetTest, IntersectTest) {
+    Set<int> set1;
+    set1.add(1);
+    set1.add(2);
+    Set<int> set2;
+    set2.add(2);
+    set2.add(3);
+    Set<int> result = set1.intersect(set2);
+    EXPECT_EQ(result.getSize(), 1);
+    EXPECT_TRUE(result.contains(2));
+}
+
+TEST(SetTest, DiffTest) {
+    Set<int> set1;
+    set1.add(1);
+    set1.add(2);
+    Set<int> set2;
+    set2.add(2);
+    set2.add(3);
+    Set<int> result = set1.diff(set2);
+    EXPECT_EQ(result.getSize(), 1);
+    EXPECT_TRUE(result.contains(1));
+}
+
+TEST(SetTest, SymDiffTest) {
+    Set<int> set1;
+    set1.add(1);
+    set1.add(2);
+    Set<int> set2;
+    set2.add(2);
+    set2.add(3);
+    Set<int> result = set1.sym_diff(set2);
+    EXPECT_EQ(result.getSize(), 2);
+    EXPECT_TRUE(result.contains(1));
+    EXPECT_TRUE(result.contains(3));
+}
+
+TEST(SetTest, PrintTest) {
+    Set<int> set;
+    set.add(1);
+    set.add(2);
+    testing::internal::CaptureStdout();
+    set.print();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Set contents: 1 2 \n");
+}
+
+TEST(SetTest, GetSizeTest) {
+    Set<int> set;
+    set.add(1);
+    set.add(2);
+    EXPECT_EQ(set.getSize(), 2);
+}
